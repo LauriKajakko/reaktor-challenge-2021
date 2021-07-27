a5457390034329442b14d32dd83e0c860494e4d4
LauriKajakko <kajakko.lauri@gmail.com>
2021-07-27 20:44:52 +0300

    refactor

--- src/utils/ruleParser.js
+++ src/utils/ruleParser.js
@@ -1,29 +1,30 @@
 const title = /^\d{1,2}\..+/;
+const firstTitle = /^1\.\s.*/;
+const firstChapter = /100\.\s.*/;
+const firstRule = /100\.1.*/;
 
-const chapterToArray = (text, chapter) => {
-  const regexp = new RegExp(`^${chapter}\\.\\d(\\d|\\.|[a-z]).*`, 'gm');
-  const rulesArray = text.match(regexp);
-  return rulesArray || [];
-};
-
-const firstIndex = (rulesarray) => rulesarray
-  .findIndex((line, index, array) => (
-    line.match(/^1\.\s.*/)
-    && array[index + 1].match(/100\.\s.*/)
-    && array[index + 2].match(/100\.1.*/)
-  ));
-
-const lastIndex = (rulesarray) => rulesarray
-  .reduce((acc, curr, index) => {
-    if (!title.test(curr)) return index;
-    return acc;
-  }, 0);
+const startsRulesList = (line, index, array) => (
+  firstTitle.test(line)
+  && firstChapter.test(array[index + 1])
+  && firstRule.test(array[index + 2])
+);
 
 const tableOfContentsToArray = (text) => {
-  const matches = text.match(/(^\d+\..*)/gm);
-  const slicedTop = matches.slice(firstIndex(matches));
-  const slicedBottom = slicedTop.slice(0, lastIndex(slicedTop));
-  return slicedBottom.filter((line) => /(^\d+\.\s.*)/.test(line));
+  const firstIndex = (rulesarray) => rulesarray.findIndex(startsRulesList);
+
+  const lastIndex = (rulesarray) => rulesarray
+    .reduce((acc, curr, index) => (!title.test(curr) ? index : acc), 0);
+
+  const enumeratedLines = text.match(/(^\d+\..*)/gm);
+  return enumeratedLines
+    .slice(firstIndex(enumeratedLines), lastIndex(enumeratedLines))
+    .filter((line) => /(^\d+\.\s.*)/.test(line));
+};
+
+const chapterToArray = (text, chapter) => {
+  const rule = new RegExp(`^${chapter}\\.\\d(\\d|\\.|[a-z]).*`, 'gm');
+  const rulesArray = text.match(rule);
+  return rulesArray || [];
 };
 
 export default { chapterToArray, tableOfContentsToArray };
